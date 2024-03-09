e774b91438092cc0418eb498d6e624360a58aaae
dlddu <39251873+dlddu@users.noreply.github.com>
2024-03-09 15:56:26 +0000

    add spring boot initializr

---
+++ src/main/java/com/pmyo/pmyo/PmyoApplication.java
@@ -0,0 +1,13 @@
+package com.pmyo.pmyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmyoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmyoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pmyo/pmyo/PmyoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmyo.pmyo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmyoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
