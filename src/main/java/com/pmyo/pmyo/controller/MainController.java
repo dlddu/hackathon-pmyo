4ab502ef1e2abff73d67a622854d7a09a1113221
dlddu <39251873+dlddu@users.noreply.github.com>
2024-03-10 01:55:49 +0900

    controller 폴더 이동

--- src/main/java/com/pmyo/pmyo/controller/MainController.java
+++ src/main/java/com/pmyo/pmyo/controller/MainController.java
@@ -1,13 +1,13 @@
-package com.controller;
+package com.pmyo.pmyo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("main")
 public class MainController {
-    @GetMapping("/main")
+    @GetMapping
     public String GetMain(){
         return "main";
     }
