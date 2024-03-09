572d47cd8f470e43d7d39335e60adce295d6968d
hongjungin <jungini0000@gmail.com>
2024-03-10 03:30:24 +0900

    user, recycleResult2

--- src/main/java/com/pmyo/pmyo/controller/RecycleResultController.java
+++ src/main/java/com/pmyo/pmyo/controller/RecycleResultController.java
@@ -1,8 +1,8 @@
 package com.pmyo.pmyo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pmyo.pmyo.model.RecycleResult;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 public class RecycleResultController{
 
@@ -14,6 +14,7 @@ public class RecycleResultController{
     @PostMapping("/gpts")
     public String calculateRecycleResult(@RequestParam("pass") int pass,
                                          @RequestParam("fail") int fail,
+                                         @RequestParam("score") int score,
                                          Model model) {
         RecycleResult result = new RecycleResult();
         result.setPass(pass);
--- src/main/java/com/pmyo/pmyo/controller/UserController.java
+++ src/main/java/com/pmyo/pmyo/controller/UserController.java
@@ -1,8 +1,9 @@
 package com.pmyo.pmyo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.pmyo.pmyo.model.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 public class UserController{
     @GetMapping("/")
